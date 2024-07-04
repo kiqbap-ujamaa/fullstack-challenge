9ae54036ba62a6aad61ccbcdfff2eaf2afa8d76d
System Administrator <root@caiques-mbp.lan>
2024-07-04 13:14:15 -0300

    Updating backend permissions

--- terraform/stage/backend.tf
+++ terraform/stage/backend.tf
@@ -4,7 +4,5 @@ terraform {
     key            = "environments/stage/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-    secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
   }
 }
