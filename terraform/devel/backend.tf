18a036dcba29a9d85646c1feff3ccbe3654c3583
System Administrator <root@caiques-mbp.lan>
2024-07-04 13:21:47 -0300

    Updating backend permissions

--- terraform/devel/backend.tf
+++ terraform/devel/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket         = "fullstack-challenge-state-files"
-    key            = "environments/devel/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "fullstack-challenge-state-files"
+#     key            = "environments/devel/terraform.tfstate"
+#     region         = "us-east-1"
+#     encrypt        = true
+#   }
+# }
